4a6c1a64efe15de1da185614e038364b3c56d497
Andrew Katz <aakatz3@gmail.com>
2016-12-09 18:34:42 -0500

    Minor fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TigerScoutTeleopBoost_Iterative.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TigerScoutTeleopBoost_Iterative.java
@@ -51,8 +51,7 @@ import com.qualcomm.robotcore.util.Range;
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-@Disabled
-@TeleOp(name="TigerScout: Teleop Boost (Jack and Yi's Boost)", group="Tiger Scout")
+@TeleOp(name="TigerScout: Teleop Boost - 2 Controllers (Jack and Yi's Boost)", group="Tiger Scout")
 public class TigerScoutTeleopBoost_Iterative extends OpMode{
     boolean last_lbump_state = false;
     boolean flipper_state = false;
