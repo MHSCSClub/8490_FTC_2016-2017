8e2ba7bd8e6a44b9ff6e8f3b4f797ee056c890f3
Andrew Katz <aakatz3@gmail.com>
2016-12-20 21:17:28 -0500

    Update motors to benefit other teams

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Motors.java
@@ -2,13 +2,18 @@ package org.firstinspires.ftc.teamcode;
 
 /**
  * Created by Andrew A. Katz on 12/9/2016.
+ * Helper class that contains encoder data for various motors allowed by FTC.
+ * Contains motors from AndyMark, Pitsco, ModernRobotics, and Rev Robotics.
  */
 
 @SuppressWarnings("unused")
 public final class Motors {
-    public static final int ANDYMARK_60_CPR = 1680;
-    public static final int TETRIX_CPR = 1440;
     public static final int ANDYMARK_UNGEARED_PPR = 28;
-    public static final int ANDYMARK_40_CPR = 1120;
     public static final int ANDYMARK_20_CPR = 560;
+    public static final int ANDYMARK_40_CPR = 1120;
+    public static final int ANDYMARK_60_CPR = 1680;
+    public static final int ANDYMARK_3_7_CPR = (int) 44.4;
+    public static final int TETRIX_CPR = 1440;
+    public static final int REV_HD_HEX_CPR = 2240;
+    public static final int MODERNROBOTICS_MATRIX_12V_CPR = (int) 1478.4;
 }
