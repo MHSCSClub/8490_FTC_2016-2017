19e659e3777f3b37b6e34e0c29e4d6f55cc1c66a
Andrew Katz <aakatz3@gmail.com>
2017-01-20 10:10:08 -0500

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutonomousPaulyP123.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedAutonomousPaulyP123.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
  * Created by Andrew on 1/19/2017.
  */
 @Autonomous(name="TigerScout: PaulyP123 RED AUTO", group="Tiger Scout")
-public class RedAutonomousPauly123AAA extends AutonomousPaulyP123{
+public class RedAutonomousPaulyP123 extends AutonomousPaulyP123{
 
     @Override
     public boolean redOrBlue() {
